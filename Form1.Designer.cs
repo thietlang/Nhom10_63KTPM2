60b9c07eff9f7befe2578c399e5e765037cefd14
thietlang <110318687+thietlang@users.noreply.github.com>
2023-11-30 15:34:38 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace tientrinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Show_Process = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Show_Process
+            // 
+            this.Show_Process.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Show_Process.Location = new System.Drawing.Point(106, 176);
+            this.Show_Process.Name = "Show_Process";
+            this.Show_Process.Size = new System.Drawing.Size(157, 72);
+            this.Show_Process.TabIndex = 0;
+            this.Show_Process.Text = "Hiển thị danh sách tiến trình";
+            this.Show_Process.UseMnemonic = false;
+            this.Show_Process.UseVisualStyleBackColor = true;
+            this.Show_Process.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(651, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 72);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Đóng file Word";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(382, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 72);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Mở file Word";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(382, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 65);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Thay đổi ảnh nền màn hình";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Show_Process);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Quản lý tiến trình";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Show_Process;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tientrinh
+{
+    public partial class Form1 : Form
+    {
+        private Process wordProcess; // Tham chiếu để lưu trữ tiến trình Word
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowRunningProcesses();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            StartNewProcess("winword.exe");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Đóng tiến trình Word nếu nó đã được mở
+            if (wordProcess != null && !wordProcess.HasExited)
+            {
+                wordProcess.CloseMainWindow();
+                wordProcess.WaitForExit();
+            }
+            else
+            {
+                MessageBox.Show("Microsoft Word is not running.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void ShowRunningProcesses()
+        {
+            // Lấy danh sách các tiến trình đang chạy
+            Process[] processes = Process.GetProcesses();
+
+            // Hiển thị thông tin về từng tiến trình trong cửa sổ mới
+            StringBuilder processInfo = new StringBuilder();
+            foreach (Process process in processes)
+            {
+                processInfo.AppendLine($"Process Name: {process.ProcessName}, ID: {process.Id}");
+            }
+
+            // Hiển thị thông tin trong một cửa sổ mới
+            ShowProcessesForm(processInfo.ToString(), "Running Processes");
+        }
+
+        private void ShowProcessesForm(string content, string title)
+        {
+            // Tạo một cửa sổ mới
+            Form processesForm = new Form();
+            processesForm.Text = title;
+
+            // Tạo một TextBox để hiển thị thông tin và cho phép cuộn
+            TextBox textBox = new TextBox
+            {
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+                ReadOnly = true,
+                Dock = DockStyle.Fill,
+                Text = content
+            };
+
+            // Thêm TextBox vào Form
+            processesForm.Controls.Add(textBox);
+
+            // Thiết lập kích thước của Form
+            processesForm.Size = new System.Drawing.Size(800, 500);
+
+            // Hiển thị cửa sổ
+            processesForm.ShowDialog();
+        }
+
+        private void StartNewProcess(string processName)
+        {
+            try
+            {
+                // Tạo một tiến trình mới
+                wordProcess = new Process();
+
+                // Thiết lập thông tin của tiến trình
+                wordProcess.StartInfo.FileName = processName;
+
+                // Khởi động tiến trình
+                wordProcess.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenPersonalizationSettings();
+        }
+        private void OpenPersonalizationSettings()
+        {
+            try
+            {
+                // Mở cửa sổ Personalization trong Settings
+                Process.Start("ms-settings:personalization-background");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening Personalization Settings: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
